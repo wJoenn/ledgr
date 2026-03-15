2720c8467cbebf002918a029a230736ef18f34b5
Louis Ramos <louisramosdev@gmail.com>
2026-03-15 11:22:46 +0100

    feat(utils): create FilteredAttrs type

--- packages/utils/src/types/index.d.ts
+++ packages/utils/src/types/index.d.ts
@@ -1,1 +1,5 @@
+export type FilteredAttrs<T, F extends Primitive> = {
+  [K in keyof T]: T[K] extends F ? K : never
+}[keyof T];
+
 export type Primitive = boolean | null | number | string | undefined;
