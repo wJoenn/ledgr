5066c78b100e4210107454e3c6b19b99eb014b7f
Louis Ramos <louisramosdev@gmail.com>
2026-03-15 16:42:31 +0100

    feat(nuxt): auto-import @ledgr/ui components

--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -1,7 +1,7 @@
 import type { ModuleDependencyMeta } from '@nuxt/schema';
 import type { ModuleOptions as TailwindModuleOptions } from '@nuxtjs/tailwindcss';
 
-import { addImportsSources, defineNuxtModule } from '@nuxt/kit';
+import { addComponentExports, addImportsSources, defineNuxtModule } from '@nuxt/kit';
 
 export type ModuleOptions = Record<string, never>;
 
@@ -14,11 +14,16 @@ export default defineNuxtModule<ModuleOptions>({
       version: '^6.0.0',
       defaults: {
         configPath: '@ledgr/tailwind-config',
+        config: {
+          content: ['./node_modules/@ledgr/nuxt/node_modules/@ledgr/ui/**/*.vue'],
+        },
       },
     },
   },
-  setup () {
+  setup: () => {
     // Remove alias after https://github.com/nuxt/nuxt/pull/34582
     addImportsSources({ package: '@ledgr/utils' } as unknown as Parameters<typeof addImportsSources>[0]);
+
+    addComponentExports({ filePath: '@ledgr/ui' });
   },
 });
