22fc7292e44cd376481b4d537ff1617433562cfa
Louis Ramos <louisramosdev@gmail.com>
2026-03-15 11:21:56 +0100

    feat(utils): create Array#groupBy

---
+++ packages/utils/src/utils/array/groupBy.ts
@@ -0,0 +1,31 @@
+import type { FilteredAttrs } from '../../types';
+
+/**
+ * Groups the collection by the given attribute.
+ *
+ * @param records An array of records.
+ * @param attr An attribute of the given records.
+ * @returns A map where the keys are the attribute's values and the values are arrays of records with matching value.
+ *
+ * @example
+ * const records = [{ id: 1, foo: 'bar' }, { id: 2, foo: 'bar' }, { id: 3, foo: 'biz' }];
+ * const grouped = groupBy(records, 'foo');
+ *
+ * console.log(grouped);
+ * // Output: {
+ * //   bar: [{ id: 1, foo: 'bar' }, { id: 2, foo: 'bar' }],
+ * //   biz: [{ id: 3, foo: 'biz' }]
+ * // }
+ */
+export const groupBy = <
+  T extends Record<string, unknown>,
+  A extends FilteredAttrs<T, number | string>,
+>(records: T[], attr: A): Record<Extract<T[A], PropertyKey>, T[]> => (
+  records.reduce((groups, currentRecord) => {
+    const key = currentRecord[attr] as Extract<T[A], PropertyKey>;
+    groups[key] ||= [];
+    groups[key].push(currentRecord);
+
+    return groups;
+  }, {} as Record<Extract<T[A], PropertyKey>, T[]>)
+);
--- packages/utils/src/utils/array/index.ts
+++ packages/utils/src/utils/array/index.ts
@@ -1,3 +1,4 @@
+export { groupBy } from './groupBy.ts';
 export { pluck } from './pluck.ts';
 export { sum } from './sum.ts';
 export { sumBy } from './sumBy.ts';
